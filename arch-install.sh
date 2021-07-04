cf8b9b9e449b3a063ce59f00b0b227d85c478ffc
Your Name <you@example.com>
2021-07-04 20:50:56 +0800

    update arch install script

--- arch-install.sh
+++ arch-install.sh
@@ -9,12 +9,17 @@ echo "owpk" > /etc/hostname
 echo "127.0.0.1	localhost" >> /etc/hosts
 echo "::1	localhost" >> /etc/hosts
 echo "127.0.1.1   owpk.localdomain  owpk" >> /etc/hosts
+echo "enter root password:"
 passwd
+echo "Creating user 'owpk'..."
 useradd -g users -G wheel,storage,power -m owpk
+echo "enter owpk password:"
 passwd owpk
 chmod +w /etc/sudoers
 echo "owpk ALL=(ALL) ALL" >> /etc/sudoers
-pacman -S networkmanager network-manager-applet wireless_tools wpa_supplicant dialog reflector mtools vim neovim snapper os-prober grub efibootmgr
+pacman -S networkmanager --needed network-manager-applet bluez bluez-utils wireless_tools wpa_supplicant dialog reflector mtools vim neovim snapper os-prober grub efibootmgr
 systemctl enable NetworkManager.service
+systemctl enable bluetooth.service
+echo 'GRUB_DISABLE_OS_PROBER=false' >> /etc/default/grub
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
