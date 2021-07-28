b54f27d543f614b5dabd04de8c0815979a2e0b7f
owpk <vzvz4@mail.ru>
2021-07-28 20:26:49 +0800

    update

--- .config/sway/scripts/startup_config.sh
+++ .config/sway/scripts/startup_config.sh
@@ -5,6 +5,6 @@ CONF="$HOME/.config/sway/keyboard.conf"
 
 echo "# keyboard settings was preloaded by $HOME/.config/sway/scipts/startup_config.sh script" > $TARGET
 echo "# you need to reboot system if you start sway with this dotfiles for the first time, because input config will be loaded
-before this script is executed"
+before this script is executed" >> $TARGET
 echo "input $KEYBOARD {" >> $TARGET
 cat $CONF >> $TARGET
