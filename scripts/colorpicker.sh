fd10c55f7ec9ed807a689ce0f29e88094e080a3f
owpk <vzvz4@mail.ru>
2021-08-09 10:02:49 +0800

    waybar update

--- scripts/colorpicker.sh
+++ scripts/colorpicker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-grim -g "$(slurp -p)" -t ppm - | convert - -format '%[pixel:p{0,0}]' txt:-
+grim -g "$(slurp -p)" - -t png -o | convert png:- -format '%[pixel:s]\n' info:- | awk -F '[(,)]' '{printf("#%02x%02x%02x\n",$2,$3,$4)}'
