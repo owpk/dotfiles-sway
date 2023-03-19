baf17a9b97009ade773a2b32b5e4fa75641e0c8c
Vyacheslav Vorobev <voyzvz@gmail.com>
2023-03-19 13:35:23 +0800

    MASTER: typescript nvim

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -55,7 +55,7 @@ return packer.startup(function(use)
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
-  -- use "williamboman/nvim-lsp-installer" -- simple to use language server installer
+  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use "ray-x/lsp_signature.nvim"
   use "SmiteshP/nvim-navic"
