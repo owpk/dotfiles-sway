9a0c43a7b43cd1adecce6b8aa2661f2f84b87f43
owpk <voyzvz@gmail.com>
2023-11-03 14:09:29 +0800

    MASTER: fix translation

--- .config/sway/scripts/translate.sh
+++ .config/sway/scripts/translate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # translate-shell dependency required !
 
-txt=$(wl-paste | trans -b -t $1)
-echo $txt
-wl-copy $txt
+txt=$(wl-paste | trans -b -t "$1")
+echo "$txt"
+wl-copy "$txt"
 notify-send "Translatrion: " "${txt}"  --urgency=critical
